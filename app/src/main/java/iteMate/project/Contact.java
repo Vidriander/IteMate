448f4a73bd7012cfd7ec8857d3e309a8c832b654
Vidriander <david.brundert@gmail.com>
2024-08-23 10:12:39 +0200

    fixed crash in Track by making Contact and Track implement Parcelable instead of Serializable

--- app/src/main/java/iteMate/project/Contact.java
+++ app/src/main/java/iteMate/project/Contact.java
@@ -1,10 +1,13 @@
 package iteMate.project;
-import java.io.Serializable;
+
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Contact class to store the details of a contact
  */
-public class Contact implements Serializable {
+public class Contact implements Parcelable {
+
     private String firstName;
     private String lastName;
     private String phone;
@@ -76,4 +79,40 @@ public class Contact implements Serializable {
         this.city = city;
     }
 
+    // Parcelable implementation
+    protected Contact(Parcel in) {
+        firstName = in.readString();
+        lastName = in.readString();
+        phone = in.readString();
+        email = in.readString();
+        address = in.readString();
+        city = in.readString();
+    }
+
+    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel in) {
+            return new Contact(in);
+        }
+
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+        dest.writeString(phone);
+        dest.writeString(email);
+        dest.writeString(address);
+        dest.writeString(city);
+    }
 }
--- app/src/main/java/iteMate/project/Track.java
+++ app/src/main/java/iteMate/project/Track.java
@@ -1,4 +1,9 @@
 package iteMate.project;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.Date;
@@ -6,7 +11,8 @@ import java.util.Date;
 /**
  * Track class to store detail of a lend out item.
  */
-public class Track implements Serializable {
+public class Track implements Parcelable {
+
     private Date giveOutDate;
     private Date returnDate;
     private Contact contact;
@@ -64,6 +70,40 @@ public class Track implements Serializable {
 
     public void removeLendList(Item item) {
         LendList.remove(item);
+    }
+
+    public static final Creator<Track> CREATOR = new Creator<Track>() {
+        @Override
+        public Track createFromParcel(Parcel in) {
+            return new Track(in);
+        }
+
+        @Override
+        public Track[] newArray(int size) {
+            return new Track[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(giveOutDate != null ? giveOutDate.getTime() : -1);
+        dest.writeLong(returnDate != null ? returnDate.getTime() : -1);
+        dest.writeParcelable(contact, flags);
+        dest.writeTypedList(LendList);
+    }
+
+    protected Track(Parcel in) {
+        long giveOutDateLong = in.readLong();
+        giveOutDate = giveOutDateLong != -1 ? new Date(giveOutDateLong) : null;
+        long returnDateLong = in.readLong();
+        returnDate = returnDateLong != -1 ? new Date(returnDateLong) : null;
+        contact = in.readParcelable(Contact.class.getClassLoader());
+        LendList = in.createTypedArrayList(Item.CREATOR);
     }
 
 }
