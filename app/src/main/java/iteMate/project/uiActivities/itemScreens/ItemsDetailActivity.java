0c83474c2b56a9d04193b93b8ffd548ffede9129
idlehand <trefzer.christian@gmail.com>
2024-08-24 01:46:23 +0200

    still working on images not being displayed...

--- app/src/main/java/iteMate/project/uiActivities/itemScreens/ItemsDetailActivity.java
+++ app/src/main/java/iteMate/project/uiActivities/itemScreens/ItemsDetailActivity.java
@@ -20,9 +20,11 @@ import java.util.List;
 
 import iteMate.project.models.Item;
 import iteMate.project.R;
+import iteMate.project.repositories.ItemRepository;
 import iteMate.project.uiActivities.utils.ContainedItemAdapter;
+import iteMate.project.uiActivities.utils.ItemAdapter;
 
-public class ItemsDetailActivity extends AppCompatActivity {
+public class ItemsDetailActivity extends AppCompatActivity  implements ItemRepository.OnItemsFetchedListener {
 
     private Item itemToDisplay;
     private RecyclerView horizontalRecyclerView;
@@ -52,11 +54,20 @@ public class ItemsDetailActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Initilize ItemRepository
+        ItemRepository itemRepository = new ItemRepository();
+
         // Initialize Item list
         itemList = new ArrayList<>();
-        itemList.add(new Item(111111, "Rose Backroad", "Description 1", "XXXXXXX", true, null));
-        itemList.add(new Item(222222, "Nukeproof Digger", "Description 2", "XXXXXXXX", true, null));
-        itemList.add(new Item(333333, "Cube Nuroad", "Description 3", "XXXXXXX", true, null));
+        itemList.add(new Item(111111, "Rose Backroad", "Description 1", "https://firebasestorage.googleapis.com/v0/b/itematedb-f0396.appspot.com/o/itemImages%2Fbikepacking.jpg?alt=media&token=824e1bc4-84cc-4f91-8a48-32358d47f91a", true, null));
+        itemList.add(new Item(222222, "Nukeproof Digger", "Description 2", "https://firebasestorage.googleapis.com/v0/b/itematedb-f0396.appspot.com/o/itemImages%2Frose_bike.jpg?alt=media&token=860ef9d4-a586-470f-9e10-166efe1ba067", true, null));
+        itemList.add(new Item(333333, "Cube Nuroad", "Description 3", "https://firebasestorage.googleapis.com/v0/b/itematedb-f0396.appspot.com/o/itemImages%2Fbikepacking.jpg?alt=media&token=824e1bc4-84cc-4f91-8a48-32358d47f91a", true, null));
+
+        //  Glide is configured to load images from the given paths
+        Glide.with(this)
+                .load(itemToDisplay.getImagePath())
+                .into((ImageView) findViewById(R.id.item_detailcard_image));
+
 
         horizontalRecyclerView = findViewById(R.id.itemdetailview_containeditems_recyclerview);
         horizontalRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
@@ -75,5 +86,12 @@ public class ItemsDetailActivity extends AppCompatActivity {
         } else {
             Log.e("ItemsDetailActivity", "itemToDisplay is null in setDetailViewContents");
         }
+    }
+
+    @Override
+    public void onItemsFetched(List<Item> items) {
+        itemList.clear();
+        itemList.addAll(items);
+        horizontalAdapter.notifyDataSetChanged();
     }
 }
