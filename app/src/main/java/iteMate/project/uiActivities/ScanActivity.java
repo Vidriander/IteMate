adddd24a0b0e36f58e2e42d0890b711ac76094d1
idlehand <trefzer.christian@gmail.com>
2024-09-12 19:15:28 +0200

    bug fix: items and tracks crash the app

--- app/src/main/java/iteMate/project/uiActivities/ScanActivity.java
+++ app/src/main/java/iteMate/project/uiActivities/ScanActivity.java
@@ -18,7 +18,14 @@ public class ScanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan);
 
         // Resolve the context of the intent
-        context = getIntent().getExtras().getInt("IntentOrigins");
+        // context = getIntent().getExtras().getInt("IntentOrigins");
+        // causes crash, used this instead:
+        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey("IntentOrigins")) {
+            context = getIntent().getExtras().getInt("IntentOrigins");
+        } else {
+            // Handle the case where the key does not exist
+            context = -1; // or any default value
+        }
 
         // Set functionality of close button
         findViewById(R.id.close_nfcscan).setOnClickListener(v -> finish());
