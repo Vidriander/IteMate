9b95435d8902881f57b6cdc6758d7ec064fe6d91
idlehand <trefzer.christian@gmail.com>
2024-10-31 17:20:45 +0100

    added Connectivity checker

---
+++ app/src/main/java/iteMate/project/utils/ConnectivityChecker.java
@@ -0,0 +1,31 @@
+package iteMate.project.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+
+public class ConnectivityChecker {
+    private final Context context;
+
+    public ConnectivityChecker(Context context) {
+        this.context = context;
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            Network network = cm.getActiveNetwork();
+            if (network != null) {
+                NetworkCapabilities capabilities = cm.getNetworkCapabilities(network);
+                return capabilities != null &&
+                        (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET));
+            }
+        }
+        return false;
+    }
+}
+
+
